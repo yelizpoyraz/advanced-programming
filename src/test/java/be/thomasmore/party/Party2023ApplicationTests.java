a1bfae42a666c5362261a241bd7feed9e6ad4119
marcdc-tm <marc.decaluwe@thomasmore.be>
2023-02-05 23:07:17 +0100

    init project

---
+++ src/test/java/be/thomasmore/party/Party2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.party;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Party2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
