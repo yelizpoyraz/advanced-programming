fff5b64894bb40ade4c1ce9557fdd3f078b57441
marcdc-tm <marc.decaluwe@thomasmore.be>
2023-02-05 23:30:59 +0100

    homepagina met variable

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -1,12 +1,15 @@
 package be.thomasmore.party.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping({"/", "/home"})
-    public String home() {
+    public String home(Model model) {
+        int myCalculatedValue = 34 * 62;
+        model.addAttribute("myCalculatedValue", myCalculatedValue);
         return "home";
     }
 
