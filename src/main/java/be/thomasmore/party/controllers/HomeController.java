43820e93aeeac8e1debbb82f08e9b103b6e891fc
marcdc-tm <marc.decaluwe@thomasmore.be>
2023-02-05 23:54:35 +0100

    2.6 + 2.7

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Controller
 public class HomeController {
     @GetMapping({"/", "/home"})
@@ -22,5 +25,14 @@ public class HomeController {
         model.addAttribute("street", myStreet);
         model.addAttribute("city", myCity);
         return "about";
+    }
+
+    @GetMapping("/pay")
+    public String pay(Model model) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        model.addAttribute("now", now.format(format));
+        model.addAttribute("paydate", now.plusDays(30).format(format));
+        return "pay";
     }
 }
