6ae6f32b0f42fe669f2cc7c8c36f32693f0521a0
marcdc-tm <marc.decaluwe@thomasmore.be>
2023-02-21 10:24:46 +0100

    oefeningklas

--- src/main/java/be/thomasmore/party/controllers/ClientController.java
+++ src/main/java/be/thomasmore/party/controllers/ClientController.java
@@ -1,11 +1,45 @@
 package be.thomasmore.party.controllers;
 
+import be.thomasmore.party.model.Client;
+import be.thomasmore.party.model.Venue;
 import be.thomasmore.party.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
 
 @Controller
 public class ClientController {
     @Autowired
     private ClientRepository clientRepository;
+
+    @GetMapping("/greeting")
+    public String greetingNewClient(Model model) {
+        Optional<Client> optionalClient = clientRepository.findById(1);
+        if (optionalClient.isPresent()) {
+            Client client = optionalClient.get();
+            model.addAttribute("client", client);
+            String groet = "Goedemorgen ";
+            if (client.getGender().equals("M")) {
+                groet += "meneer ";
+            } else if (client.getGender().equals("F")) {
+                groet += "mevrouw ";
+            } else {
+                groet += "beste ";
+            }
+            groet += client.getName();
+            model.addAttribute("groet", groet);
+            model.addAttribute("code", generateClientCode(client));
+        }
+        return "newclient";
+    }
+
+    private String generateClientCode(Client client) {
+        String code = "";
+        code = client.getName().substring(0, 2) +
+                client.getName().charAt(client.getName().length() - 1);
+        return code;
+    }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 INSERT INTO venue (id, venue_name, capacity, city, distance_from_public_transport_in_km, food_provided,
                    free_parking_available, indoor, outdoor, link_more_info)
 VALUES (1, 'Den Aalmoezenier', 100, 'Antwerpen', 1, true, false, true, false, 'https://denaalmoezenier.weebly.com/');
+
+INSERT INTO "PUBLIC"."CLIENT" VALUES
+    (1, '21-2-1994', 'M', 'Ernazar', '10-9-2022');
