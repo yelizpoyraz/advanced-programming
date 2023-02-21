d5764070b299c7ce923c480a9167432e38ffffcb
marcdc-tm <marc.decaluwe@thomasmore.be>
2023-02-21 11:26:47 +0100

    venuedetails/id + venuelist versie1

--- src/main/java/be/thomasmore/party/controllers/VenueController.java
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 @Controller
@@ -14,12 +16,28 @@ public class VenueController {
     @Autowired
     private VenueRepository venueRepository;
 
-    @GetMapping("/venuedetails")
-    public String venueDetails(Model model) {
-        Optional<Venue> optionalVenue = venueRepository.findById(1);
+    @GetMapping("/venuedetails/{id}")
+    public String venueDetails(Model model, @PathVariable int id) {
+        Optional<Venue> optionalVenue = venueRepository.findById(id);
         if (optionalVenue.isPresent()) {
             model.addAttribute("venue", optionalVenue.get());
         }
         return "venuedetails";
+    }
+
+    @GetMapping("/venuelist")
+    public String venueDetails(Model model) {
+        //Iterable<Venue> allVenues = venueRepository.findAll();
+        ArrayList<Venue> someVenues = new ArrayList<>();
+        Optional<Venue> optional = venueRepository.findById(2);
+        someVenues.add(optional.get());
+        optional = venueRepository.findById(5);
+        someVenues.add(optional.get());
+        optional = venueRepository.findById(7);
+        someVenues.add(optional.get());
+        for (int i=1; i <= someVenues.size(); i++) {
+            model.addAttribute("venue"+i, someVenues.get(i-1));
+        }
+        return "venuelist";
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,96 @@
-INSERT INTO venue (id, venue_name, capacity, city, distance_from_public_transport_in_km, food_provided,
-                   free_parking_available, indoor, outdoor, link_more_info)
-VALUES (1, 'Den Aalmoezenier', 100, 'Antwerpen', 1, true, false, true, false, 'https://denaalmoezenier.weebly.com/');
+insert into VENUE (
+    ID, VENUE_NAME, LINK_MORE_INFO, CAPACITY,
+    FOOD_PROVIDED, INDOOR, OUTDOOR, FREE_PARKING_AVAILABLE,
+    CITY, DISTANCE_FROM_PUBLIC_TRANSPORT_IN_KM)
+values (
+           1, 'De Roma', 'https://www.deroma.be',
+           1500, true, true, false, false, 'Antwerpen', 0.2);
+
+insert into VENUE (
+    ID, VENUE_NAME, LINK_MORE_INFO, CAPACITY,
+    FOOD_PROVIDED, INDOOR, OUTDOOR, FREE_PARKING_AVAILABLE,
+    CITY, DISTANCE_FROM_PUBLIC_TRANSPORT_IN_KM)
+values (
+           2, 'TRIX', 'https://www.trixonline.be/',
+           1100, true, true, false, false, 'Antwerpen', 0.5);
+
+insert into VENUE (
+    ID, VENUE_NAME, LINK_MORE_INFO, CAPACITY,
+    FOOD_PROVIDED, INDOOR, OUTDOOR, FREE_PARKING_AVAILABLE,
+    CITY, DISTANCE_FROM_PUBLIC_TRANSPORT_IN_KM)
+values (
+           3, 'TRIX kelder', 'https://www.trixonline.be/nl/kelder/2/',
+           120, true, true, false, false, 'Antwerpen', 0.5);
+
+insert into VENUE (
+    ID, VENUE_NAME, LINK_MORE_INFO, CAPACITY,
+    FOOD_PROVIDED, INDOOR, OUTDOOR, FREE_PARKING_AVAILABLE,
+    CITY, DISTANCE_FROM_PUBLIC_TRANSPORT_IN_KM)
+values (
+           4, 'Arenberg', 'https://www.arenberg.be',
+           800, false, true, false, false, 'Antwerpen', 4);
+
+insert into VENUE (
+    ID, VENUE_NAME, LINK_MORE_INFO, CAPACITY,
+    FOOD_PROVIDED, INDOOR, OUTDOOR, FREE_PARKING_AVAILABLE,
+    CITY, DISTANCE_FROM_PUBLIC_TRANSPORT_IN_KM)
+values (
+           5, 'Reuzenpoort', 'https://www.tilemalines.be',
+           180, false, true, true, true, 'Mechelen', 0.3);
+
+insert into VENUE (
+    ID, VENUE_NAME, LINK_MORE_INFO, CAPACITY,
+    FOOD_PROVIDED, INDOOR, OUTDOOR, FREE_PARKING_AVAILABLE,
+    CITY, DISTANCE_FROM_PUBLIC_TRANSPORT_IN_KM)
+values (
+           6, 'Zaal Athena', 'https://www.zaalathena.be',
+           250, false, true, false, false, 'Antwerpen', 0.1);
+
+
+insert into VENUE (
+    ID, VENUE_NAME, LINK_MORE_INFO, CAPACITY,
+    FOOD_PROVIDED, INDOOR, OUTDOOR, FREE_PARKING_AVAILABLE,
+    CITY, DISTANCE_FROM_PUBLIC_TRANSPORT_IN_KM)
+values (
+           7, 'OLT Rivierenhof', 'https://www.oltrivierenhof.be',
+           2500, false, false, true, true, 'Antwerpen', 1);
+
+insert into VENUE (
+    ID, VENUE_NAME, LINK_MORE_INFO, CAPACITY,
+    FOOD_PROVIDED, INDOOR, OUTDOOR, FREE_PARKING_AVAILABLE,
+    CITY, DISTANCE_FROM_PUBLIC_TRANSPORT_IN_KM)
+values (
+           8, 'Koningin Elisabethzaal', 'https://www.koninginelisabethzaal.be',
+           1850, true, true, false, false, 'Antwerpen', 0.1);
+
+insert into VENUE (
+    ID, VENUE_NAME, LINK_MORE_INFO, CAPACITY,
+    FOOD_PROVIDED, INDOOR, OUTDOOR, FREE_PARKING_AVAILABLE,
+    CITY, DISTANCE_FROM_PUBLIC_TRANSPORT_IN_KM)
+values (
+           9, 'Sportpaleis', 'https://www.sportpaleis.be',
+           23000, true, true, false, true, 'Antwerpen', 1);
+
+insert into VENUE (
+    ID, VENUE_NAME, LINK_MORE_INFO, CAPACITY,
+    FOOD_PROVIDED, INDOOR, OUTDOOR, FREE_PARKING_AVAILABLE,
+    CITY, DISTANCE_FROM_PUBLIC_TRANSPORT_IN_KM)
+values (
+           10, 'Lotto Arena', 'https://www.lotto-arena.be/',
+           8050, true, true, true, true, 'Antwerpen', 1);
+
+insert into VENUE (
+    ID, VENUE_NAME, LINK_MORE_INFO, CAPACITY,
+    FOOD_PROVIDED, INDOOR, OUTDOOR, FREE_PARKING_AVAILABLE,
+    CITY, DISTANCE_FROM_PUBLIC_TRANSPORT_IN_KM)
+values (
+           11, 'Club Cabane', 'https://www.strantwerpen.be',
+           200, false, true, true, false, 'Antwerpen', 1.2);
+
+insert into VENUE (
+    ID, VENUE_NAME, LINK_MORE_INFO, CAPACITY,
+    FOOD_PROVIDED, INDOOR, OUTDOOR, FREE_PARKING_AVAILABLE,
+    CITY, DISTANCE_FROM_PUBLIC_TRANSPORT_IN_KM)
+values (
+           12, 'Den Aalmoezenier', 'https://denaalmoezenier.weebly.com/',
+           100, true, true, false, false, 'Antwerpen', 1);
